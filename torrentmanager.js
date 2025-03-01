be9894dd44f9cde760a9c884496382f737dacfca
Anton K <wiistriker@gmail.com>
2025-03-02 00:32:09 +0300

    change script path

--- torrentmanager.js
+++ torrentmanager.js
@@ -1,6 +1,7 @@
 (function () {
     'use strict';
 
-    Lampa.Utils.putScriptAsync(['https://lampame.github.io/main/torrentmanager/torrentmanager.js'], function () { });
+    //Lampa.Utils.putScriptAsync(['https://lampame.github.io/main/torrentmanager/torrentmanager.js'], function () { });
+    Lampa.Utils.putScriptAsync(['https://wiistriker.github.io/lampame-main/torrentmanager/torrentmanager.js'], function () { });
 
 })();
